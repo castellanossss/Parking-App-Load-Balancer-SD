3cdc4561e0063f65e4fdf295a114dc0da2a49f9c
Leonardo Castellanos <jaider.castellanos@uptc.edu.co>
2024-03-10 19:54:52 -0500

    Fixed load_balancer.js

--- load_balancer.js
+++ load_balancer.js
@@ -1,7 +1,5 @@
 const http = require('http');
-const httpProxy = require('http-proxy');
-
-require('dotenv').config();
+const axios = require('axios');
 
 const servers = [
     { target: process.env.SERVER_1 },
@@ -11,21 +9,31 @@ const servers = [
 
 let currentServer = 0;
 
-const proxy = httpProxy.createProxyServer({});
-
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const target = servers[currentServer].target;
+    const clientIp = req.socket.remoteAddress;
+
+    console.log(`Solicitud del cliente ${clientIp} redirigida a ${target}`);
+
     currentServer = (currentServer + 1) % servers.length;
 
-    console.log(`Redirigiendo la solicitud a: ${target}`);
+    try {
+        const response = await axios({
+            method: req.method,
+            url: target + req.url,
+            headers: req.headers,
+            data: req.body
+        });
 
-    proxy.web(req, res, { target }, (error) => {
+        res.writeHead(response.status, response.headers);
+        res.end(response.data);
+    } catch (error) {
         console.error(`Error al conectar con el servidor ${target}: ${error.message}`);
         res.writeHead(502);
         res.end(`Error al conectar con el servidor ${target}`);
-    });
+    }
 });
 
 server.listen(8010, () => {
-    console.log('Load balancer listening on port 8000');
+    console.log('Load balancer listening on port 8010');
 });
